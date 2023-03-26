fdb1dc7a772e495fa13a23b513de4df4c972d0ce
Yasha <yashasidin333@yandex.ru>
2023-03-26 21:02:22 +0300

    Add solves of main Tasks

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -3,3 +3,21 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+// Рекурсивная сумма натуральных элементов от M до N
+int GetSumInRange(int startRange, int endRange)
+{
+    if (startRange - 1 == endRange)
+    {
+        return 0;
+    }
+
+    return endRange + GetSumInRange(startRange, endRange - 1);
+}
+
+Console.WriteLine("Введите начало диапозона: ");
+int startRange = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите конец диапозона: ");
+int endRange = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма всех элементов от {startRange} до {endRange} = {GetSumInRange(startRange, endRange)}");
--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -2,3 +2,16 @@
 // Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+// Вычисление значения функции Аккермана с помощью рекурсии
+int GetAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return GetAkkerman(m - 1, 1);
+    else return GetAkkerman(m - 1, GetAkkerman(m, n - 1));
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+Console.WriteLine(GetAkkerman(2, 3));
+Console.WriteLine(GetAkkerman(3, 2));
